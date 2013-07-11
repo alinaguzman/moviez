428d0c2d35a6ce23ef5f5e226f5e74f8828dbd9b
Alina Guzman <alinochka8@gmail.com>
2013-07-10 21:39:54 -0400

    Created migration to add actors table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710192942) do
+ActiveRecord::Schema.define(:version => 20130711013725) do
+
+  create_table "actors", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "movies", :force => true do |t|
     t.string   "name"
